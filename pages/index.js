ead0690e4b55a1c7f08f6a14a64d931a7328186e
Mojtaba <mojtaba.see@gmail.com>
2022-08-14 13:47:24 +0430

    title changed to Mojtaba Shafiee

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,14 @@ import About from '../components/UI/About';
 import Portfolio from '../components/UI/Portfolio';
 import Testimonial from '../components/UI/Testimonial';
 import Contact from '../components/UI/Contact';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <Fragment>
+      <Head>
+        <title>Mojtaba Shafiee</title>
+      </Head>
       <Hero />
       <Services />
       <About />
